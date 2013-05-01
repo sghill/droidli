6663aeeadfe76e457454eacd864fc67f7b6f0e73
sghill <sghill.dev@gmail.com>
2013-04-30 22:09:17 -0400

    querying for project contributions

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,8 @@ class Project < ActiveRecord::Base
   belongs_to :license
   has_many :project_tags
   has_many :tags, :through => :project_tags
+  has_many :project_contributions
+
   attr_accessible :in_maven_central, :name, :url, :license_id
   attr_accessor :updated_by, :tweetable
 end
